54ed12c46856fa7d0a460a6f8244de6bb30dd960
RexenN <96340446+RexenN@users.noreply.github.com>
2022-05-19 21:04:25 +0300

    first

---
+++ src/components/TripList.js
@@ -0,0 +1,41 @@
+import { useState, useEffect, useCallback } from "react"
+import './TripList.css'
+
+export default function TripList() {
+const [trips, setTrips] = useState([])
+const [url, setUrl] = useState('http://localhost:3000/trips')
+
+const fetchTrips = useCallback(async () => {
+    const response = await fetch(url)
+    const json = await response.json()
+    setTrips(json)
+}, [])
+
+useEffect(() => {
+ fetchTrips()
+}, [url, fetchTrips])
+
+console.log(trips)
+
+  return (
+    <div className="trip-list">
+        <h2>Trip List</h2>
+        <ul>
+            {trips.map(trip => (
+                <li key={trip.id}>
+                    <h3>{trip.title}</h3>
+                    <p>{trip.price}</p>
+                </li>
+            ))}
+        </ul>
+        <div className="filters">
+                <button onClick={() => setUrl('http://localhost:3000/trips?loc=europe')}> 
+                    European Trips
+                </button>
+                <button onClick={() => setUrl('http://localhost:3000/trips')}>
+                    All Trips
+                </button>
+        </div>
+    </div>
+  )
+}
