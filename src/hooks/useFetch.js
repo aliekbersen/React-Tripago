cd58731d6113731849ffb1fd6a8e4159656290f4
RexenN <96340446+RexenN@users.noreply.github.com>
2022-05-20 17:11:13 +0300

    last

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,13 +3,26 @@ import { useState, useEffect } from 'react'
 
 export const useFetch = (url) => {
     const [data, setData] = useState(null)
+    const [isPending, setIsPending] = useState(false)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
        const fetchData = async () => {
-        const res = await fetch(url)
-        const json = await res.json()
-       
-        setData(json)
+        setIsPending(true)
+
+        try { 
+            const res = await fetch(url)
+            const json = await res.json()
+           
+            setIsPending(false)
+            setData(json)
+            setError(null)
+        }catch(err){
+            setIsPending(false)
+            setError('could not fetch the data')
+            console.log(err.message)
+            }
+        
         }
         fetchData()
     }, [url])
