07c1454dbe0f0a287e7c723c5829b0f207b7d8ee
RexenN <96340446+RexenN@users.noreply.github.com>
2022-05-20 15:28:37 +0300

    error

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,27 +1,18 @@
-import { useState, useEffect, useCallback } from "react"
+import { useState } from "react"
 import './TripList.css'
+import { useFetch } from  '../hooks/useFetch'
 
 export default function TripList() {
-const [trips, setTrips] = useState([])
+
 const [url, setUrl] = useState('http://localhost:3000/trips')
+const { data: trips } = useFetch(url)
 
-const fetchTrips = useCallback(async () => {
-    const response = await fetch(url)
-    const json = await response.json()
-    setTrips(json)
-}, [])
-
-useEffect(() => {
- fetchTrips()
-}, [url, fetchTrips])
-
-console.log(trips)
 
   return (
     <div className="trip-list">
         <h2>Trip List</h2>
         <ul>
-            {trips.map(trip => (
+            {trips && trips.map(trip => (
                 <li key={trip.id}>
                     <h3>{trip.title}</h3>
                     <p>{trip.price}</p>
